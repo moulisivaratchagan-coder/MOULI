903d7b295417b9c2a8c700577922a7c5cb98f1b5
moulisivaratchagan-coder <moulisivaratchagan>
2026-02-18 13:40:21 +0530

    go

---
+++ src/test/java/com/git/Git_task2.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class Git_task2 {
+
+	private void m3() {
+		// TODO Auto-generated method stub
+
+		
+	}
+	private void m4() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
